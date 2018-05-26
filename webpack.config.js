34763830ba0d285e5aa5bf14615222dd17fb737d
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-05-26 09:36:18 -0300

    copy redirects in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,39 @@
 const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, "src/index.html"),
-  filename: "./index.html"
+  template: path.join(__dirname, 'src/index.html'),
+  filename: './index.html'
 })
 
+const fileRedirect = './_redirects'
+const addRedirect = new CopyWebpackPlugin([{
+  from: fileRedirect,
+  to: fileRedirect,
+  toType: 'file',
+}])
+
+
 module.exports = {
-  entry: path.join(__dirname, "src/index.jsx"),
+  entry: path.join(__dirname, 'src/index.jsx'),
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
-  plugins: [ htmlWebpackPlugin ],
+  plugins: [ htmlWebpackPlugin, addRedirect ],
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     port: 3001
