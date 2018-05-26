556633b518e8883c8b6ed6961bff2df588efe1a9
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-05-26 10:07:16 -0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, 'src/index.html'),
   filename: './index.html'
 })
-
-const fileRedirect = './_redirects'
-const addRedirect = new CopyWebpackPlugin([{
-  from: fileRedirect,
-  to: fileRedirect,
-  toType: 'file',
-}])
-
 
 module.exports = {
   entry: path.join(__dirname, 'src/index.jsx'),
@@ -31,7 +21,7 @@ module.exports = {
       }
     ]
   },
-  plugins: [ htmlWebpackPlugin, addRedirect ],
+  plugins: [ htmlWebpackPlugin ],
   resolve: {
     extensions: ['.js', '.jsx']
   },
