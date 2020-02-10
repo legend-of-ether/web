031af76c4fc54a8b9363ee6c3f85cd9899c47fb0
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-02-10 19:05:06 -0300

    use web3 1.2.6

--- src/index.jsx
+++ src/index.jsx
@@ -2,33 +2,48 @@ import React from 'react'
 import { render} from 'react-dom'
 import IO from 'socket.io-client'
 import contract from '@legend-of-ether/sol'
+import Web3 from 'web3'
 
 import { Ethereal } from './components/Ethereal'
-import { getAccountIdWithAttempts } from './eth/Account'
 
 import './index.css'
 
 const { SOCKET_URL } = process.env
 
 async function main() {
+  const web3 = await getWeb3Instance()
+
+  const accounts = await web3.eth.getAccounts()
+  const id = accounts[0]
+
+  console.log('accounts', accounts)
+
   const constractAddress = contract.networks['3'].address
   console.log('contract address', `https://ropsten.etherscan.io/address/${constractAddress}`)
 
-  console.log('getting eth id...')
-  const id = await getAccountIdWithAttempts()
-  console.log('got eth id', id)
+  console.log('Connecting to Socket', SOCKET_URL)
 
   const socket = IO(SOCKET_URL)
 
   socket.emit('signIn', id)
 
-  const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'))
-
-  const contractInstance = web3.eth
-    .contract(contract.abi)
-    .at(contract.networks['3'].address)
+  const contractInstance = new web3.eth.Contract(contract.abi, contract.networks['3'].address)
 
   render(<Ethereal socket={socket} myId={id} contract={contractInstance} />, document.getElementById("react-root"))
+}
+
+async function getWeb3Instance() {
+  // TODO: use 'https://ropsten.infura.io/v3/25915a33d3264e5d975f8759231b9d10' with MetaMask?
+  if (window.ethereum) {
+    const web3 = new Web3(ethereum);
+    await ethereum.enable();
+    return web3
+  }
+
+  if (window.web3)
+    return new Web3(web3.currentProvider);
+
+  throw new Error()
 }
 
 main().catch(console.error)
